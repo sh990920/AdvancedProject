bb6777a8e7b65b7b05bce1b63e3ee519c46ca014
sh990920 <sh990920@gmail.com>
2024-08-01 20:57:58 +0900

    SnapKit 추가, TabBarController 설정

---
+++ AdvancedProject/BasketViewController.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class BasketViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .yellow
+    }
+    
+    // 전체 삭제
+    
+    // 담은 책
+    
+    // 추가
+    // UIBarButtonItem 으로 추가하면 될듯
+    
+    // 추가 리스트
+        // 제목
+        // 작가
+        // 가격
+    
+    
+}
---
+++ AdvancedProject/DetailViewController.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+class DetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+    
+    // 디테일 모달
+    
+    // 책 제목
+    
+    // 작가
+    
+    // 책 사진
+    
+    // 가격
+    
+    // 내용
+    
+    // x 버튼 과 담기 버튼
+    
+}
